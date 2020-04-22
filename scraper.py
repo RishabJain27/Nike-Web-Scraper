ec9934de0648835f25a0f6e6ed464a4013a244de
Rishab Jain <rjain9@ucsc.edu>
2020-04-22 16:22:31 -0700

    new scraper

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import json
 from pymongo import MongoClient
+import sys
+sys.stdout = open('file', 'w', encoding="utf-8")
 
 
 url = "https://www.nike.com/w/new-shoes-3n82yzy7ok"
@@ -16,36 +18,29 @@ driver.get(url)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 
 #connect to database
-client = MongoClient("mongodb+srv://<user>:<password>@cluster0-wgm3y.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://<username>:<password>@cluster0-wgm3y.mongodb.net/test?retryWrites=true&w=majority")
 db = client["Shoes"]
 mycol = db["nike"]
 
-#find all img tags and take out unnecessary 
-aTagsInLi = driver.find_elements_by_css_selector('img')
-aTagsInLi.pop()
-aTagsInLi.pop()
-del aTagsInLi[0:2] 
-
-#array for json objects
+#find every div with class name
+aTagsInLi = driver.find_elements_by_class_name('product-card__img-link-overlay')
 line_items=[]
-#loop through all elements found and add them to array
 for a in aTagsInLi:
-    myjson3 = {
-                'name': a.get_attribute('alt'),
-                'image_url': a.get_attribute('src'),
-            }
+    #print(a.get_attribute('href'))
+    img = a.find_element_by_tag_name('img')
 
-    str = a.get_attribute('alt')
-    if str.find("Shoe") > 0:
-    	line_items.append(myjson3)
-    	
-    #print("img link: ", a.get_attribute('src'), " name: ", a.get_attribute('alt'))
+    myjson3 = {
+                'name': img.get_attribute('alt'),
+                'image_url': img.get_attribute('src'),
+                'site': a.get_attribute('href')
+            }
+    line_items.append(myjson3)
+    #print("img link: ", img.get_attribute('src'), " name: ", img.get_attribute('alt'))
 
 #clear existing db
 mycol.delete_many({})
 #insert new elements into db
 mycol.insert_many(line_items)
-
 
     
 
