ab452332b391aae5c6778520d492ea88ff84634b
Sonali Jain <rjain9@ucsc.edu>
2023-09-20 19:47:22 -0700

    update div tags and update driver func to new selenium functions

--- scraper.py
+++ scraper.py
@@ -2,38 +2,37 @@
 import urllib.request
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import json
 from pymongo import MongoClient
 import sys
 import time
 sys.stdout = open('file', 'w', encoding="utf-8")
 
-
-url = "https://www.nike.com/w/new-shoes-3n82yzy7ok"
-# run firefox webdriver from executable path of your choice
-driver = webdriver.Firefox()
-
 # get web page
+url = "https://www.nike.com/w/new-shoes-3n82yzy7ok"
+driver = webdriver.Firefox()
 driver.get(url)
+
 # execute script to scroll down the page
 driver.maximize_window()
 time.sleep(5)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 
 #connect to database
-client = MongoClient("mongodb+srv://rjain9:Ilikepie16%21@cluster0-wgm3y.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://rjain9:Ilikepie16@sneakers.1azjgzw.mongodb.net/?retryWrites=true&w=majority")
+
 db = client["Shoes"]
 mycol = db["nike"]
-aTagsInLi = driver.find_elements_by_xpath("//div[@class='product-card css-1ikfoht css-z5nr6i css-11ziap1 css-zk7jxt css-dpr2cn product-grid__card ']")
+aTagsInLi = driver.find_elements("xpath", "//div[@class='product-card product-grid__card  css-1t0asop']")
                                                           
 line_items=[]
 for a in aTagsInLi:
     
-    print("here")
     #get div container for image details
-    img = a.find_element_by_tag_name('img')
+    img = a.find_element(By.TAG_NAME,'img')
     #get div for site line
-    siteDiv = a.find_element_by_tag_name('a')
+    siteDiv = a.find_element(By.TAG_NAME, 'a')
 
     #get name of shoe
     name = img.get_attribute('alt')
@@ -42,7 +41,7 @@ for a in aTagsInLi:
     #get site link
     site = siteDiv.get_attribute('href')
     #get category of shoe
-    category = a.find_element_by_class_name('product-card__subtitle').text
+    category = a.find_element("xpath", "//div[@class='product-card__subtitle']").text
 
     #determine gender
     if "Men" in category:
@@ -70,9 +69,3 @@ for a in aTagsInLi:
 mycol.delete_many({})
 #insert new elements into db
 mycol.insert_many(line_items)
-
-    
-
-
-
-
